61a8482f024829e3b67dd1bb0873f93020586115
Fillipi Pereira <pfillipi@gmx.de>
2022-01-15 01:57:41 +0100

    substring(0, 12)

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -59,7 +59,7 @@ const Products = () => {
               <div class="card h-100 text-center p-4" key={product.id}>
                 <img src={product.image} class="card-img-top" alt={product.title} height="250px" />
                 <div class="card-body">
-                  <h5 class="card-title mb-0">{product.title}…</h5>
+                  <h5 class="card-title mb-0">{product.title.substring(0, 12)}…</h5>
                   <p class="card-text lead fw-bold">${product.price}</p>
                   <a href="#" class="btn btn-outline-dark">Buy</a>
                 </div>
