61ff62e5e7ad46dbd0081ec7bce893f1c3103423
Fillipi Pereira <pfillipi@gmx.de>
2022-01-24 05:42:26 +0100

    Reducer

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,6 +1,13 @@
 export const addCart = (product) => {
   return {
-    type: "ADDCART",
+    type: "ADDITEM",
+    payload: product,
+  };
+};
+
+export const delCart = (product) => {
+  return {
+    type: "DELITEM",
     payload: product,
   };
 };
